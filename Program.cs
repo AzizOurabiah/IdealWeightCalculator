2a648f8e1c5f15b7ce5853ab61f49d90e4cc8801
Aziz Ourabiah <ourabiahaziz1@gmail.com>
2022-05-20 11:42:13 -0400

    Initiation du projet

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IdealWeightCalculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeightCalculator wc = new WeightCalculator()
+            {
+                Height = 180,
+                Sex = 'm'                
+            };
+            double weight = wc.GetIdealBodyWeight();
+
+            Console.WriteLine($"The ideal body is : {weight}", weight);
+
+            if(weight == 172.5)
+            {
+                //Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Test succedes !");
+            }
+            else
+            {
+                //Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Test failes !");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WeightCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdealWeightCalculator1
+{
+    public class WeightCalculator
+    {
+        public double Height { get; set; }
+        public char Sex { get; set; }
+
+        public double GetIdealBodyWeight()
+        {
+            switch (Sex)
+            {
+                case 'm':
+                    return (Height - 100) - ((Height - 150)) / 4;
+                case 'w':
+                    return (Height - 100) - ((Height - 150) / 2);
+                default:
+                    return 0;
+            }          
+        }
+    }
+}
